e313b5161c93a8a1105fe61f782e4327d44d1001
harika208 <harikahn119@gmail.com>
2022-12-23 17:59:40 +0530

    commit

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ let meridian;
    }
 
    
-
-
-   let hour1 = document.getElementsByClassName("hour12");
+    let hour1 = document.getElementsByClassName("hour12");
     hour1[0].innerHTML = `${hours}`
 
     let min1 = document.getElementsByClassName("min12");
@@ -48,35 +46,28 @@ let meridian;
     meridian1[0].innerHTML = meridian;
 
     let good12 = document.getElementsByClassName("good1");
-    let image = document.getElementsByClassName("morning");
-    let two12 = document.getElementsByClassName("two1");
     
    var good1 = " ";
-    var two1 = " ";
     if((hours < 12) && (meridian == "AM") ){
           good1 = good1 + "GOOD MORNING!! WAKE UP!!";
-          two1 = two1 + "GRAB SOME HEALTHY BREAKFAST!!!"; 
-          image[0].src = "./wind.jpg";
+        
            
     }
      else if((hours == 12) || (hours < 4) && (meridian == "PM")){
       good1 = good1 + "GOOD AFTERNOON!! TAKE SOME SLEEP";
-        two1 = two1 + "LET'S HAVE SOME LUNCH !!";
-        image[0].src = "./lunchpic.jpg";
+    
          
      }
      else if((hours >= 4) && (hours <= 7) && (meridian == "PM")){
       good1 = good1 + "GOOD EVENING !!";
-        two1 = two1 + "STOP YAWNING, GET SOME TEA... ITS JUST EVENNG! "; 
-        image[0].src = "./coffee.jpg";
+    
          
      }
      else{
-        two1 = two1 + "CLOSE YOUR EYES AND GO TO SLEEP";
-         image[0].src = "./goodnigth.jpg";
+        
+    
          good1 = good1 + "GOOD NIGHT !!"; 
      }
-     two12[0].innerText = two1;
      good12[0].innerHTML = good1;
 
     
@@ -88,21 +79,52 @@ setInterval(() => {
 },1000);
 
 function alaram(){
+   
    let wake = document.getElementsByClassName("last1");
    let wake1 = document.querySelector("#morn").value;
    wake[0].innerHTML = `Wake Up Time: ${wake1}`;
+
+   if(wake1 !== "Default"){
+      let image = document.getElementsByClassName("morning");
+        image[0].src = "./wind.jpg";
+        let two12 = document.getElementsByClassName("two1");
+         two12[0].innerText = "GRAB SOME HEALTHY BREAKFAST!!!"
+  }
 
    let after1 = document.getElementsByClassName("last2");
    let after12 = document.querySelector("#after").value;
    after1[0].innerHTML = `Lunch Time: ${after12}`;
 
+      if(after12 !== "Default"){
+      let image = document.getElementsByClassName("morning");
+        image[0].src = "./lunchpic.jpg";
+        let two12 = document.getElementsByClassName("two1");
+        two12[0].innerText = "LET'S HAVE SOME LUNCH !!";
+  
+     }
+
    let even1 = document.getElementsByClassName("last3");
    let even12 = document.querySelector("#even").value;
    even1[0].innerHTML = `Nap Time: ${even12}`;
 
+     if(even12 !== "Default"){
+       let image = document.getElementsByClassName("morning");
+        image[0].src = "./coffee.jpg";
+        let two12 = document.getElementsByClassName("two1");
+         two12[0].innerText = "STOP YAWNING, GET SOME TEA... ITS JUST EVENNG! ";
+    }
+
+
    let night1 = document.getElementsByClassName("last4");
    let night12 = document.querySelector("#night").value;
    night1[0].innerHTML = `Night Time: ${night12}`;
-   
+       if(night12 !== "Default"){
+       let image = document.getElementsByClassName("morning");
+      image[0].src = "./goodnigth.jpg";
+      let two12 = document.getElementsByClassName("two1");
+      two12[0].innerText = "CLOSE YOUR EYES AND GO TO SLEEP";
+    }
+
+  
 }
 alaram();
